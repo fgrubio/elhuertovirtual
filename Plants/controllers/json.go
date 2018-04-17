d00095aa172069e60115e562e99dd6db6e7887fa
David <davidvalero01@gmail.com>
2018-04-17 14:39:59 +0200

    json updare

--- Plants/controllers/json.go
+++ Plants/controllers/json.go
@@ -22,9 +22,22 @@ func (c *JSONController) Get() {
 		}*/
 	//var w io.Writer
 	//json.NewEncoder(w).Encode(todos)
-
+	var taula []models.Plantas
+	models.DB.Table("plantas").Select("id,deleted_at,tipo,cantidad,duracion,seleccio").Scan(&taula)
 	var mystruct models.Todos
-	models.DB.Table("plantas").Select("tipo,cantidad,duracion,seleccio").Scan(&mystruct)
+	for i := 0; i < len(taula); i++ {
+		if taula[i].DeletedAt == nil {
+			var taulainsert models.Todo
+			taulainsert.Tipo = taula[i].Tipo
+			taulainsert.Cantidad = taula[i].Cantidad
+			taulainsert.Duracion = taula[i].Duracion
+			taulainsert.Seleccio = taula[i].Seleccio
+
+			mystruct = append(mystruct, taulainsert)
+		}
+	}
+	//var mystruct models.Todos
+	//models.DB.Table("plantas").Select("tipo,cantidad,duracion,seleccio").Scan(&mystruct)
 	c.Data["json"] = &mystruct
 	c.ServeJSON()
 }
